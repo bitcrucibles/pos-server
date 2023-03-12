26bb504ae035b1f4079d188567ee54be08a75bd3
lp1 <lp1@bitcrucibles.com>
2023-03-13 10:33:56 +1100

    Fix dev simnet

--- docker/lspd/start.sh
+++ docker/lspd/start.sh
@@ -1,17 +1,6 @@
 sleep 20s
 ./generate_macaroon_hex LND /root/breez_node/data/chain/bitcoin/simnet/admin.macaroon /root/breez_node/tls.cert >> .env
 
-cat .env
-
-source .env
-
-echo NODES=\'[ {\"name\": \"Breez\", \"nodePubkey\": \"080e22595863892dfcff0fdc1b2d7e53f521a90dca692ebfd173c17be841f237\", \"lspdPrivateKey\": \"$LSPD_PRIVATE_KEY\", \"token\": \"$TOKEN\", \"host\": \"$LISTEN_ADDRESS\", \"publicChannelAmount\": \"1000183\", \"channelAmount\": \"100000\", \"channelPrivate\": false, \"targetConf\": \"6\", \"minHtlcMsat\": \"600\", \"baseFeeMsat\": \"1000\", \"feeRate\": \"0.000001\", \"timeLockDelta\": \"144\", \"channelFeePermyriad\": \"40\", \"channelMinimumFeeMsat\": \"2000000\", \"additionalChannelCapacity\": \"100000\", \"maxInactiveDuration\": \"3888000\", \"lnd\": { \"address\": \"$LND_ADDRESS\", \"cert\": \"$LND_CERT\", \"macaroon\": \"$LND_MACAROON_HEX\" } } ]\' \
->> .env
-
-echo "#### cat .env second time:"
-
-cat .env
-
 
 /go/bin/migrate   -source file:///go/lspd/postgresql/migrations/ --database postgres://postgres:test@10.5.0.8:5432/postgres?sslmode=disable up 10
 #wait for breez rpc
@@ -20,4 +9,19 @@ do
     sleep 1
     echo "lspd waiting for breez RPC..."
 done
+
+#Get node pub key
+source .env
+
+/go/bin/lncli --rpcserver=$LND_ADDRESS --macaroonpath=/root/breez_node/data/chain/bitcoin/simnet/admin.macaroon --tlscertpath=/root/breez_node/tls.cert -network=simnet getinfo
+export LND_PUB_KEY=$(/go/bin/lncli --rpcserver=$LND_ADDRESS --macaroonpath=/root/breez_node/data/chain/bitcoin/simnet/admin.macaroon --tlscertpath=/root/breez_node/tls.cert -network=simnet getinfo | jq -r '.identity_pubkey')
+echo "LND_PUB_KEY: $LND_PUB_KEY"
+
+echo NODES=\'[ {\"name\": \"Breez\", \"nodePubkey\": \"$LND_PUB_KEY\", \"lspdPrivateKey\": \"$LSPD_PRIVATE_KEY\", \"token\": \"$TOKEN\", \"host\": \"$LISTEN_ADDRESS\", \"publicChannelAmount\": \"1000183\", \"channelAmount\": \"100000\", \"channelPrivate\": false, \"targetConf\": \"6\", \"minHtlcMsat\": \"600\", \"baseFeeMsat\": \"1000\", \"feeRate\": \"0.000001\", \"timeLockDelta\": \"144\", \"channelFeePermyriad\": \"40\", \"channelMinimumFeeMsat\": \"2000000\", \"additionalChannelCapacity\": \"100000\", \"maxInactiveDuration\": \"3888000\", \"lnd\": { \"address\": \"$LND_ADDRESS\", \"cert\": \"$LND_CERT\", \"macaroon\": \"$LND_MACAROON_HEX\" } } ]\' \
+>> .env
+
+echo "#### cat .env:"
+
+cat .env
+
 exec /go/bin/godotenv ./lspd/lspd
--- docker/start-dev-env.sh
+++ docker/start-dev-env.sh
@@ -1,15 +1,16 @@
-export DEV_HOST_IP=127.0.0.1
-export TEST_DIR=/Users/roeierez/test/docker
+export DEV_HOST_IP=192.168.0.8
+export TEST_DIR=/home/deniv/tmp/test/docker-volumes
 
-export ALICE_BREEZ_ADDRESS="127.0.0.1:50053"
+export ALICE_BREEZ_ADDRESS="192.168.0.8:50053"
 export ALICE_DIR=$TEST_DIR/alice_node
-export ALICE_LND_ADDRESS="127.0.0.1:10009"
+export ALICE_LND_ADDRESS="192.168.0.8:10009"
 export BREEZ_DIR=$TEST_DIR/breez_node
-export BREEZ_LND_ADDRESS="127.0.0.1:10010"
+export BREEZ_LND_ADDRESS="192.168.0.8:10010"
 export SUBSWAP_DIR=$TEST_DIR/subswap_node
-export SUBSWAP_LND_ADDRESS="127.0.0.1:10012"
-export BTCD_HOST="127.0.0.1:18556"
+export SUBSWAP_LND_ADDRESS="192.168.0.8:10012"
+export BTCD_HOST="192.168.0.8:18556"
 export BTCD_CERT_FILE=$TEST_DIR/btcd-rpc.cert
+export DEBUG="debug"
 
 rm -rf $TEST_DIR
 mkdir $TEST_DIR
@@ -33,15 +34,23 @@ docker-compose -f dev-simnet.yml run -d --name dev-breez dev-breez
 until docker exec dev-breez "cat" /root/.lnd/logs/bitcoin/simnet/lnd.log | grep 'RPC server listening on' > /dev/null;
 do
     sleep 1
-    #echo "waiting for breez RPC..."
+    echo "waiting for breez RPC..."
 done
 docker exec dev-breez "/lnd/lncli" -network=simnet newaddress np2wkh | jq -r '.address'
 export MINING_ADDRESS=$(docker exec dev-breez "/lnd/lncli" -network=simnet newaddress np2wkh | jq -r '.address')
+echo "Mining Address: $MINING_ADDRESS"
 docker exec dev-btcd cat /rpc/rpc.cert > $TEST_DIR/btcd-rpc.cert
 
+docker stop dev-breez
+docker rm dev-breez
+
 # restart containers
+echo "Restart containers"
 docker-compose -f dev-simnet.yml down
+echo "dev-simnet is down"
 docker-compose -f dev-simnet.yml up -d
-docker exec dev-btcd /start-btcctl.sh generate 400
+echo "dev-simnet is up"
+docker exec dev-btcd /start-btcctl.sh generate 10
+echo "BTCD: generated 400"
 
 #go test ../itest/tests
